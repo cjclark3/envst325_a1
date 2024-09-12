b4422f526fa93a2be493b47dcf9d9fbc9569f19f
cjclark3 <cj.clark2021@gmail.com>
2024-09-12 00:49:52 +0000

    homework 1

--- activity1.R
+++ activity1.R
@@ -27,3 +27,32 @@ highPeaks <- data.frame(elev = peaks,
                         name = peakNames)
 highPeaks[1:1]
 highPeaks[1:2]
+
+#in-class prompt 1
+#The average snow depth over the past several days was 2.5, 3, 5, and 4.5 inches. 
+#Create a vector of observations and convert these observations to centimeters.
+snow.depth.in <- c(2.5, 3, 5, 4.5)
+
+#in-class prompt 2
+#Below is a table of several of the most difficult high peaks to summit. 
+#Enter the information in the table into a data frame.
+high.peaks <- data.frame(
+  name = c("Mount Haystack", "Mount Skylight", "Dix Mountain", "Gray Peak"),
+  elevation.ft = c(4960, 4926, 4857, 4840),
+  climb.ascent.ft = c(3570, 4265, 2800, 4178),
+  roundtrip.length.mi = c(17.8, 17.9, 13.2, 16))
+
+#in-class prompt 3
+#Practice mathematical operations in R.
+#Convert the following observations from Celsius to Fahrenheit: -44, 0, 20, 35.
+celsius <- c(-44, 0, 20, 35)
+fahrenheit <- celsius * (9/5) + 32
+
+#hw question 1
+#Convert the elevation of the high peaks data frame (from above) from feet to meters in R.
+high.peaks$elevation.m <- high.peaks$elevation.ft / 3.281
+
+#hw question 3
+#Create a variable that isolates the row with the longest round trip length.
+longest.roundtrip <- high.peaks[which.max(high.peaks$roundtrip.length.mi),]
+
